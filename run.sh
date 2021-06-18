31cca4189f531f0b3c4537aca74861113f103ae5
Nikolay Beresnev <elektron0583@gmail.com>
2021-06-19 00:16:10 +0700

    Update run.sh
    Fix script

--- run.sh
+++ run.sh
@@ -10,25 +10,26 @@ fi
 if [ ! -f "/opt/btfs/config" ]; then
     if [ $NEW_WALLET == true ]; then
         RUN BTFS_PATH="/opt/btfs" /usr/bin/btfs init
+        status=$?
     else if [ -n $MNEMONIC_WORDS ]; then
         RUN BTFS_PATH="/opt/btfs" /usr/bin/btfs init -s $MNEMONIC_WORDS
+        status=$?
     else if [ -n $PRIVATE_KEY ]; then
         RUN BTFS_PATH="/opt/btfs" /usr/bin/btfs init -i $PRIVATE_KEY
+        status=$?
     else
         RUN BTFS_PATH="/opt/btfs" /usr/bin/btfs init
+        status=$?
     fi
 
     BTFS_PATH="/opt/btfs" /usr/bin/btfs --api /ip4/0.0.0.0/tcp/5001 daemon
     status=$?
-    if [ $status -ne 0 ]; then
-        echo "Failed to start btfs: $status"
-        exit $status
-    fi
 else
     BTFS_PATH="/opt/btfs" /usr/bin/btfs --api /ip4/0.0.0.0/tcp/5001 daemon
     status=$?
-    if [ $status -ne 0 ]; then
-        echo "Failed to start btfs: $status"
-        exit $status
-    fi
+fi
+
+if [ $status -ne 0 ]; then
+    echo "Failed to start btfs: $status"
+    exit $status
 fi
