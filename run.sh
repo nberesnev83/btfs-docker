083650294b7e1982319916fcf433322546424eda
Nikolay Beresnev <elektron0583@gmail.com>
2021-06-19 02:56:44 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -24,6 +24,23 @@ if [[ ! -f "/opt/btfs/config" ]]; then
 
     ENABLE_WALLET_REMOTE=true BTFS_PATH="/opt/btfs" /usr/bin/btfs --api /ip4/0.0.0.0/tcp/5001 daemon
     status=$?
+
+    if [[ -n "$DOMAINAPI" ]]; then
+        ENABLE_WALLET_REMOTE=true BTFS_PATH="/opt/btfs" /usr/bin/btfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["http://$DOMAINAPI:5001", "http://0.0.0.0:5001"]'
+    else
+        ENABLE_WALLET_REMOTE=true BTFS_PATH="/opt/btfs" /usr/bin/btfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["http://0.0.0.0:5001"]'
+    fi
+    ENABLE_WALLET_REMOTE=true BTFS_PATH="/opt/btfs" /usr/bin/btfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", "POST"]'
+
+    if [[ -n "$WALLET_PASSWORD" ]]; then
+        ENABLE_WALLET_REMOTE=true BTFS_PATH="/opt/btfs" /usr/bin/btfs wallet password --password="$WALLET_PASSWORD"
+    fi
+
+    if [[ "$ENABLE_STORAGE" == "true" ]]; then
+        ENABLE_WALLET_REMOTE=true BTFS_PATH="/opt/btfs" /usr/bin/btfs config profile apply storage-host
+    fi
+
+    killall -9 btfs
 else
     ENABLE_WALLET_REMOTE=true BTFS_PATH="/opt/btfs" /usr/bin/btfs --api /ip4/0.0.0.0/tcp/5001 daemon
     status=$?
