e1b6b40e8e8d13e8408cf06a5d548f10c7c7903f
Nikolay Beresnev <elektron0583@gmail.com>
2021-06-18 23:05:20 +0700

    Update run.sh
    Fix run ssh & add --api

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-/usr/sbin/sshd -D
+/usr/sbin/sshd
 status=$?
 if [ $status -ne 0 ]; then
     echo "Failed to start sshd: $status"
     exit $status
 fi
 
-BTFS_PATH="/opt/btfs" /usr/bin/btfs daemon
+BTFS_PATH="/opt/btfs" /usr/bin/btfs --api /ip4/0.0.0.0/tcp/5001 daemon
 status=$?
 if [ $status -ne 0 ]; then
-    echo "Failed to start apache2ctl: $status"
+    echo "Failed to start btfs: $status"
     exit $status
 fi
