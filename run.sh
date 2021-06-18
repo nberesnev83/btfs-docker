f8c2ac9be93a2ab8718a14c1f64cb38240f158db
Nikolay Beresnev <elektron0583@gmail.com>
2021-06-19 01:19:55 +0700

    Update run.sh
    Fix error

--- run.sh
+++ run.sh
@@ -2,20 +2,20 @@
 
 /usr/sbin/sshd
 status=$?
-if [ $status -ne 0 ]; then
+if [[ $status -ne 0 ]]; then
     echo "Failed to start sshd: $status"
     exit $status
 fi
 
-if [ ! -f "/opt/btfs/config" ]; then
-    if [ $NEW_WALLET == true ]; then
+if [[ ! -f "/opt/btfs/config" ]]; then
+    if [[ "$NEW_WALLET" == "true" ]]; then
         RUN BTFS_PATH="/opt/btfs" /usr/bin/btfs init
         status=$?
-    else if [ -n $MNEMONIC_WORDS ]; then
-        RUN BTFS_PATH="/opt/btfs" /usr/bin/btfs init -s $MNEMONIC_WORDS
+    else if [[ -n "$MNEMONIC_WORDS" ]]; then
+        RUN BTFS_PATH="/opt/btfs" /usr/bin/btfs init -s "$MNEMONIC_WORDS"
         status=$?
-    else if [ -n $PRIVATE_KEY ]; then
-        RUN BTFS_PATH="/opt/btfs" /usr/bin/btfs init -i $PRIVATE_KEY
+    else if [[ -n "$PRIVATE_KEY" ]]; then
+        RUN BTFS_PATH="/opt/btfs" /usr/bin/btfs init -i "$PRIVATE_KEY"
         status=$?
     else
         RUN BTFS_PATH="/opt/btfs" /usr/bin/btfs init
@@ -26,12 +26,12 @@ if [ ! -f "/opt/btfs/config" ]; then
     status=$?
 fi
 
-if [ -f "/opt/btfs/config" ]; then
+if [[ -f "/opt/btfs/config" ]]; then
     BTFS_PATH="/opt/btfs" /usr/bin/btfs --api /ip4/0.0.0.0/tcp/5001 daemon
     status=$?
 fi
 
-if [ $status -ne 0 ]; then
+if [[ $status -ne 0 ]]; then
     echo "Failed to start btfs: $status"
     exit $status
 fi
